68ce99129a00ce3a99b53c37db4d48a3d95a09cb
yihug <31957705+DanielChungYi@users.noreply.github.com>
2025-03-04 13:16:33 +0800

    [src]: clean up

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -40,34 +40,14 @@ func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, r, "home.page.tmpl", &models.TemplateData{})
 }
 
-// About is the handler for the about page
-func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
-	// perform some logic
-	stringMap := make(map[string]string)
-	stringMap["test"] = "Hello, again"
-
-	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
-	stringMap["remote_ip"] = remoteIP
-
-	// send data to the template
-	render.RenderTemplate(w, r, "about.page.tmpl", &models.TemplateData{
-		StringMap: stringMap,
-	})
-}
-
 // Reservation renders the make a reservation page and displays form
 func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{})
 }
 
-// Generals renders the room page
-func (m *Repository) Generals(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, r, "generals.page.tmpl", &models.TemplateData{})
-}
-
-// Majors renders the room page
-func (m *Repository) Majors(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, r, "majors.page.tmpl", &models.TemplateData{})
+// Court information
+func (m *Repository) CourtInfo(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, r, "court-info.page.tmpl", &models.TemplateData{})
 }
 
 // Availability renders the search availability page
@@ -113,22 +93,6 @@ func (m *Repository) PostAvailability(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Respon JSON:", string(out))
 
 	//w.Write([]byte(fmt.Sprintf("Date is %s and time is %s", data, time)))
-}
-
-// AvailabilityJSON handles request for availability and sends JSON response
-func (m *Repository) AvailabilityJSON(w http.ResponseWriter, r *http.Request) {
-	resp := jsonResponse{
-		OK:      true,
-		Message: "Available!",
-	}
-
-	out, err := json.MarshalIndent(resp, "", "     ")
-	if err != nil {
-		log.Println(err)
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(out)
 }
 
 // Contact renders the contact page
